4a2722fc18c8e1e3070064d2ee811493fa8e118a
张勇豪 <1104380920@qq.com>
2025-05-11 00:24:44 +0800

    feature: 定义分布式缓存接口方法

--- admin/src/main/java/com/luobida/houserice/admin/common/cache/DistributedCache/DistributedCache.java
+++ admin/src/main/java/com/luobida/houserice/admin/common/cache/DistributedCache/DistributedCache.java
@@ -1,7 +1,27 @@
 package com.luobida.houserice.admin.common.cache.DistributedCache;
 
+import com.alibaba.fastjson2.JSON;
 import com.luobida.houserice.admin.common.cache.ICache;
+import com.luobida.houserice.admin.toolkit.FastJson2Util;
+import lombok.NonNull;
 
+import java.util.concurrent.TimeUnit;
+
+/**
+ * 抽象分布式缓存接口
+ */
 public interface DistributedCache extends ICache {
+    public <T> T get(@NonNull String key, Class<T> clazz);
 
+    public void put(@NonNull String key, Object value);
+
+    public void put(String key, Object value, long timeout);
+
+    public void put(String key, Object value, long timeout, TimeUnit timeUnit);
+
+    public Boolean delete(@NonNull String key);
+
+    public Boolean hasKey(@NonNull String key);
+
+    public Object getInstance();
 }
