5be9710832f55c7680c0c8f8d61c915f3049d5a5
张勇豪 <1104380920@qq.com>
2025-05-12 19:54:57 +0800

    feature: 主类添@ConfigurationPropertiesScan 注解

--- admin/src/main/java/com/luobida/houserice/admin/HouseRiceAdminApplication.java
+++ admin/src/main/java/com/luobida/houserice/admin/HouseRiceAdminApplication.java
@@ -3,9 +3,12 @@ package com.luobida.houserice.admin;
 import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.properties.ConfigurationPropertiesScan;
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
 
 @SpringBootApplication
 @MapperScan("com.luobida.houserice.admin.dao.mapper")
+@ConfigurationPropertiesScan
 public class HouseRiceAdminApplication {
     public static void main(String[] args) {
         SpringApplication.run(HouseRiceAdminApplication.class, args);
